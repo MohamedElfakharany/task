9257b736a55f8e9110bbb6b3e9919455a5c6e3bf
MohamedElfakharany <mohmedelfakharany@gmail.com>
2022-01-06 11:50:43 +0200

    Initial Commit

---
+++ task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  task
+//
+//  Created by Mohamed Alfakharany on 06/01/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
